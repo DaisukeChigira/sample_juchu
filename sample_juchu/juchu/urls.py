1da11d830e16e6d974b1368a527a086806aa0a28
DaisukeChigira <52981047+DaisukeChigira@users.noreply.github.com>
2019-07-19 13:41:54 +0900

    Add files via upload
    チュートリアル３まで

--- sample_juchu/juchu/urls.py
+++ sample_juchu/juchu/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('<int:order_id>/', views.detail, name='detail'),
+    path('<int:product_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    path('<int:order_id>/results/', views.results, name='results'),
+    path('<int:product_id>/results/', views.results, name='results'),
     # ex: /polls/5/order/
-    path('<int:order_id>/order/', views.order, name='order'),
+    path('<int:product_id>/product/', views.order, name='product'),
 ]
--- sample_juchu/juchu/views.py
+++ sample_juchu/juchu/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Order, Product
 
 # Create your views here.
 def index(request):
     latest_question_list = Product.objects.order_by('-name')[:5]
-    output = ', '.join([q.name for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'juchu/index.html', context)
     
-def detail(request, order_id):
-    return HttpResponse("You're looking at question %s." % order_id)
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'juchu/detail.html', {'product': product})
 
-def results(request, order_id):
+def results(request, product_id):
     response = "You're looking at the results of question %s."
-    return HttpResponse(response % order_id)
+    return HttpResponse(response % product_id)
 
-def order(request, order_id):
-    return HttpResponse("You're voting on question %s." % order_id)
+def order(request, product_id):
+    return HttpResponse("You're voting on question %s." % product_id)
